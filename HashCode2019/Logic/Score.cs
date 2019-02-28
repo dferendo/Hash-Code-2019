bd78e8b4446b589d22345a3fc4862ff5a0d14b42
Kevin Napoli <kvnnap@gmail.com>
2019-02-28 20:31:52 +0100

    asdasdas

--- HashCode2019/Logic/Score.cs
+++ HashCode2019/Logic/Score.cs
@@ -7,7 +7,7 @@ namespace HashCode2019.Logic
 {
     public static class Score
     {
-        static int ComputeScore(Slide s1, Slide s2)
+        public static int ComputeScore(Slide s1, Slide s2)
         {
             var tags1 = s1.Photos.SelectMany(p => p.Tags).Distinct().ToList();
             var tags2 = s2.Photos.SelectMany(p => p.Tags).Distinct().ToList();
@@ -16,7 +16,7 @@ namespace HashCode2019.Logic
             return Math.Min(Math.Min(tags1.Count, tags2.Count), mergedTags.Count);
         }
 
-        static int ComputeScore(List<Slide> s)
+        public static int ComputeScore(List<Slide> s)
         {
             var total = 0;
             for(var i = 0; i < s.Count - 1; ++i)
