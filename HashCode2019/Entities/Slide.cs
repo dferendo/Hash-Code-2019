c27853e3ade90d9a23c75ba54db7af5693ddcab9
Kevin Napoli <kvnnap@gmail.com>
2019-02-28 20:34:18 +0100

    Rach change

--- HashCode2019/Entities/Slide.cs
+++ HashCode2019/Entities/Slide.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace HashCode2019.Entities
 {
     public class Slide
     {
         public List<Photo> Photos { get; set; }
+
+        public List<string> Tags
+        {
+            get
+            {
+                return Photos.SelectMany(photo => photo.Tags).Distinct().ToList();
+            }
+        }
     }
 }
